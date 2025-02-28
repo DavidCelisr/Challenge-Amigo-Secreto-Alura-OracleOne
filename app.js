ef4ffc49a1c0fa6b442982233ee0a4af68ac1e49
David Celis <raulorifre@gmail.com>
2025-02-27 23:03:53 -0500

    se crean variables y una funcion

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Variables
+
+let nombreAmigo = [];
+console.log(nombreAmigo); 
+
+function agregarAmigo() {
+    let inputIngresoAmigo = document.getElementById("amigo").value;
+    nombreAmigo.push(inputIngresoAmigo);
+    console.log(nombreAmigo);
+}
