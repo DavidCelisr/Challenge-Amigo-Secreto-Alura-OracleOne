7e080cb62cde480cb596509de8c43cd3e3110ed0
David Celis <raulorifre@gmail.com>
2025-03-03 10:16:05 -0500

    se agregan funciones y validaciones

--- app.js
+++ app.js
@@ -5,8 +5,47 @@
 let nombreAmigo = [];
 console.log(nombreAmigo); 
 
+//funcion agregar y validacion datos entrada
 function agregarAmigo() {
-    let inputIngresoAmigo = document.getElementById("amigo").value;
+    let inputIngresoAmigo = document.getElementById("amigo").value.trim();
+    let ingresoVacio = inputIngresoAmigo;
+    
+    if (ingresoVacio === "" || !isNaN(inputIngresoAmigo) || /\d/.test(inputIngresoAmigo)) {
+        alert("Por favor, inserte un nombre.");
+        return;
+    } 
+    
     nombreAmigo.push(inputIngresoAmigo);
     console.log(nombreAmigo);
+    renderizaAmigos();
+    
+    limpiar();
 }
+
+//funcion renderizar amigos lista
+function renderizaAmigos() {
+    let lisAmigos = document.getElementById("listaAmigos");
+    lisAmigos.innerHTML = "";
+    
+    for (let i = 0; i < nombreAmigo.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = nombreAmigo[i];
+        lisAmigos.appendChild(li);
+    }
+}
+
+//funcion limpiar caja de texto
+function limpiar() {
+    document.getElementById("amigo").value = "";
+}
+
+
+//funcion sortear amigos 
+
+
+
+
+
+
+
+
